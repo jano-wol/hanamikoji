e5ab43d737949b976e1dc6478895c99d9de94e92
Janos Wolosz <janos.wolosz@gmail.com>
2025-04-26 16:01:04 +0200

    Steps toward move generation

--- hanamikoji/env/move_generator.py
+++ hanamikoji/env/move_generator.py
@@ -5,18 +5,39 @@ class MovesGener(object):
     This is for generating the possible combinations
     """
     def __init__(self, cards_list, choose_1_2, choose_2_2):
+        self.stash_1 = None
+        self.trash_2 = None
+        self.choose_1_2 = None
+        self.choose_2_2 = None
+        self.resolve_1_2 = None
+        self.resolve_2_2 = None
+
+        if choose_2_2:
+            self.resolve_2_2 = [[[], []], [[], []]]
+            self.resolve_2_2[0][0] = choose_2_2[0]
+            self.resolve_2_2[0][1] = choose_2_2[1]
+            self.resolve_2_2[1][0] = choose_2_2[1]
+            self.resolve_2_2[1][1] = choose_2_2[0]
+            return
+
+        if choose_1_2:
+            self.resolve_1_2 = []
+            for i, val in enumerate(choose_1_2):
+                if val != 0:
+                    a = [0] * 7
+                    b = choose_1_2[:]
+                    a[i] = 1
+                    b[i] -= 1
+                    self.resolve_1_2.append([a, b])
+            return
+
+
+
         self.cards_list = cards_list
         self.cards_dict = collections.defaultdict(int)
 
         for i in self.cards_list:
             self.cards_dict[i] += 1
-
-        self.stash_1 = []
-        self.trash_2 = []
-        self.choose_1_2 = []
-        self.choose_2_2 = []
-        self.resolve_1_2 = []
-        self.resolve_2_2 = []
 
     # generate all possible moves from given cards
     def gen_moves(self):
