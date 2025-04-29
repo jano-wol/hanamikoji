6e249d4b1865a49811e483a4d412d906011c12d8
Janos Wolosz <janos.wolosz@gmail.com>
2025-04-29 21:36:27 +0200

    Improve evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -4,14 +4,11 @@ import argparse
 from hanamikoji.evaluation.simulation import evaluate
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                    'Hanamikoji Evaluation')
-    parser.add_argument('--landlord', type=str,
-            default='baselines/douzero_ADP/landlord.ckpt')
-    parser.add_argument('--landlord_up', type=str,
-            default='baselines/sl/landlord_up.ckpt')
-    parser.add_argument('--landlord_down', type=str,
-            default='baselines/sl/landlord_down.ckpt')
+    parser = argparse.ArgumentParser('Hanamikoji Evaluation')
+    parser.add_argument('--first', type=str,
+            default='baselines/hanamikojizero/first.ckpt')
+    parser.add_argument('--second', type=str,
+            default='baselines/hanamikojizero/second.ckpt')
     parser.add_argument('--eval_data', type=str,
             default='eval_data.pkl')
     parser.add_argument('--num_workers', type=int, default=5)
@@ -21,8 +18,7 @@ if __name__ == '__main__':
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_device
 
-    evaluate(args.landlord,
-             args.landlord_up,
-             args.landlord_down,
+    evaluate(args.first,
+             args.second,
              args.eval_data,
              args.num_workers)
