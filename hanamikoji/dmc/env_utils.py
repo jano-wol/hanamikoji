921f1c5e38771c898c488def595d94971bf54f55
Janos Wolosz <janos.wolosz@gmail.com>
2025-04-29 09:02:56 +0200

    Adapt env_utils

--- hanamikoji/dmc/env_utils.py
+++ hanamikoji/dmc/env_utils.py
@@ -11,44 +11,42 @@ def _format_observation(obs, device):
     A utility function to process observations and
     move them to CUDA.
     """
-    position = obs['position']
+    acting_player_id = obs['id']
+    round_id = obs['round_id']
     if not device == "cpu":
         device = 'cuda:' + str(device)
     device = torch.device(device)
     x_batch = torch.from_numpy(obs['x_batch']).to(device)
     z_batch = torch.from_numpy(obs['z_batch']).to(device)
-    x_no_action = torch.from_numpy(obs['x_no_action'])
     z = torch.from_numpy(obs['z'])
     obs = {'x_batch': x_batch,
            'z_batch': z_batch,
-           'legal_actions': obs['legal_actions'],
+           'moves': obs['moves'],
            }
-    return position, obs, x_no_action, z
+    return acting_player_id, round_id, obs, z
 
 class Environment:
     def __init__(self, env, device):
-        """ Initialzie this environment wrapper
+        """
+        Initialzie this environment wrapper
         """
         self.env = env
         self.device = device
         self.episode_return = None
 
     def initial(self):
-        initial_position, initial_obs, x_no_action, z = _format_observation(self.env.reset(), self.device)
-        initial_reward = torch.zeros(1, 1)
+        acting_player_id, round_id, initial_obs, z = _format_observation(self.env.reset(), self.device)
         self.episode_return = torch.zeros(1, 1)
         initial_done = torch.ones(1, 1, dtype=torch.bool)
 
-        return initial_position, initial_obs, dict(
+        return acting_player_id, round_id, initial_obs, dict(
             done=initial_done,
             episode_return=self.episode_return,
-            obs_x_no_action=x_no_action,
-            obs_z=z,
+            obs_z=z
             )
         
     def step(self, action):
         obs, reward, done, _ = self.env.step(action)
-
         self.episode_return += reward
         episode_return = self.episode_return 
 
@@ -56,16 +54,11 @@ class Environment:
             obs = self.env.reset()
             self.episode_return = torch.zeros(1, 1)
 
-        position, obs, x_no_action, z = _format_observation(obs, self.device)
-        reward = torch.tensor(reward).view(1, 1)
+        acting_player_id, round_id, obs, z = _format_observation(obs, self.device)
         done = torch.tensor(done).view(1, 1)
         
-        return position, obs, dict(
+        return acting_player_id, round_id, obs, dict(
             done=done,
             episode_return=episode_return,
-            obs_x_no_action=x_no_action,
-            obs_z=z,
+            obs_z=z
             )
-
-    def close(self):
-        self.env.close()
