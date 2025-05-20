50d92c93151c66e1583145bf1fa225030860ad81
Janos Wolosz <janos.wolosz@gmail.com>
2025-05-20 19:47:57 +0200

    Ease game.h

--- cpp/src/game.h
+++ cpp/src/game.h
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
-#include <map>
 #include <memory>
 #include <random>
 #include <string>
@@ -37,18 +36,16 @@ struct PrivateInfoSet
 
 struct GameState
 {
-  std::string acting_player_id = "first";
-  std::map<std::string, std::string> id_to_round_id = {{"first", "first"}, {"second", "second"}};
+  int acting_player_id = 0;
+  std::vector<int> id_to_round_id = {0, 1};
   std::vector<int> points = {2, 2, 2, 3, 3, 4, 5};
-  std::map<std::string, std::vector<int>> gift_cards = {{"first", std::vector<int>(7, 0)},
-                                                        {"second", std::vector<int>(7, 0)}};
-  std::map<std::string, std::vector<int>> action_cards = {{"first", {1, 1, 1, 1}}, {"second", {1, 1, 1, 1}}};
-  std::vector<int> decision_cards_1_2;
-  std::pair<std::vector<int>, std::vector<int>> decision_cards_2_2;
-  std::map<std::string, std::vector<int>> geisha_preferences = {{"first", std::vector<int>(7, 0)},
-                                                                {"second", std::vector<int>(7, 0)}};
-  std::map<std::string, int> num_cards = {{"first", 7}, {"second", 6}};
-  std::map<std::string, std::vector<std::pair<int, std::vector<int>>>> round_moves = {{"first", {}}, {"second", {}}};
+  std::vector<std::vector<int>> gift_cards = {std::vector<int>(7, 0), std::vector<int>(7, 0)};
+  std::vector<std::vector<int>> action_cards = {{1, 1, 1, 1}, {1, 1, 1, 1}};
+  std::vector<int> decision_cards_1_2 = {};
+  std::pair<std::vector<int>, std::vector<int>> decision_cards_2_2 = {};
+  std::vector<std::vector<int>> geisha_preferences = {std::vector<int>(7, 0), std::vector<int>(7, 0)};
+  std::vector<int> num_cards = {7, 6};
+  std::vector<std::vector<std::pair<int, std::vector<int>>>> round_moves = {{{}}, {{}}};
 };
 
 class Player
@@ -60,10 +57,10 @@ public:
 class GameEnv
 {
 public:
-  GameEnv(std::map<std::string, std::shared_ptr<Player>> players) : players(players), round(1), winner("")
+  GameEnv(std::vector<std::shared_ptr<Player>> players) : players(players), round(1), winner(-1)
   {
-    private_info_sets["first"] = PrivateInfoSet();
-    private_info_sets["second"] = PrivateInfoSet();
+    private_info_sets.push_back(PrivateInfoSet());
+    private_info_sets.push_back(PrivateInfoSet());
     state = GameState();
   }
 
@@ -87,27 +84,27 @@ public:
 
   void update_geisha_preferences()
   {
-    std::vector<int> first_gifts = add_cards(state.gift_cards["first"], private_info_sets["first"].stashed_card);
-    std::vector<int> second_gifts = add_cards(state.gift_cards["second"], private_info_sets["second"].stashed_card);
+    std::vector<int> first_gifts = add_cards(state.gift_cards[0], private_info_sets[0].stashed_card);
+    std::vector<int> second_gifts = add_cards(state.gift_cards[1], private_info_sets[1].stashed_card);
 
     for (int i = 0; i < 7; ++i) {
       if (first_gifts[i] > second_gifts[i] ||
-          (first_gifts[i] == second_gifts[i] && state.geisha_preferences["first"][i] == 1)) {
-        state.geisha_preferences["first"][i] = 1;
+          (first_gifts[i] == second_gifts[i] && state.geisha_preferences[0][i] == 1)) {
+        state.geisha_preferences[0][i] = 1;
       } else {
-        state.geisha_preferences["first"][i] = 0;
+        state.geisha_preferences[0][i] = 0;
       }
 
       if (first_gifts[i] < second_gifts[i] ||
-          (first_gifts[i] == second_gifts[i] && state.geisha_preferences["second"][i] == 1)) {
-        state.geisha_preferences["second"][i] = 1;
+          (first_gifts[i] == second_gifts[i] && state.geisha_preferences[1][i] == 1)) {
+        state.geisha_preferences[1][i] = 1;
       } else {
-        state.geisha_preferences["second"][i] = 0;
+        state.geisha_preferences[1][i] = 0;
       }
     }
   }
 
-  std::string is_game_ended()
+  int is_game_ended()
   {
     int first_geisha_win = 0;
     int first_geisha_points = 0;
@@ -115,31 +112,31 @@ public:
     int second_geisha_points = 0;
 
     for (int i = 0; i < 7; ++i) {
-      if (state.geisha_preferences["first"][i] == 1) {
+      if (state.geisha_preferences[0][i] == 1) {
         first_geisha_win++;
         first_geisha_points += state.points[i];
       }
-      if (state.geisha_preferences["second"][i] == 1) {
+      if (state.geisha_preferences[1][i] == 1) {
         second_geisha_win++;
         second_geisha_points += state.points[i];
       }
     }
 
     if (first_geisha_points >= 11)
-      return "first";
+      return 0;
     if (second_geisha_points >= 11)
-      return "second";
+      return 1;
     if (first_geisha_win >= 4)
-      return "first";
+      return 0;
     if (second_geisha_win >= 4)
-      return "second";
-    return "";
+      return 1;
+    return -1;
   }
 
   void set_winner()
   {
-    std::string winner_player = is_game_ended();
-    if (!winner_player.empty()) {
+    int winner_player = is_game_ended();
+    if (winner_player != -1) {
       winner = winner_player;
       num_wins[winner]++;
     }
@@ -147,8 +144,8 @@ public:
 
   void step()
   {
-    std::string curr = state.acting_player_id;
-    std::string opp = get_opp();
+    int curr = state.acting_player_id;
+    int opp = get_opp();
     auto info = private_info_sets[curr];
     int move_index = players[curr]->act(state, info);
     auto move = active_player_info_set.second.moves[move_index];
@@ -210,26 +207,25 @@ public:
       break;
     }
 
-    if (state.round_moves["first"].size() + state.round_moves["second"].size() == 12) {
-      assert(state.num_cards["first"] == 0 && state.num_cards["second"] == 0);
+    if (state.round_moves[0].size() + state.round_moves[1].size() == 12) {
+      assert(state.num_cards[0] == 0 && state.num_cards[1] == 0);
 
       update_geisha_preferences();
       set_winner();
 
-      if (winner.empty()) {
+      if (winner == -1) {
         auto next_geisha_preferences = state.geisha_preferences;
         round += 1;
         state = GameState();
         state.geisha_preferences = next_geisha_preferences;
 
         if (round % 2 == 0) {
-          state.acting_player_id = "second";
-          state.id_to_round_id = {{"first", "second"}, {"second", "first"}};
-          state.num_cards = {{"first", 6}, {"second", 7}};
+          state.acting_player_id = 1;
+          state.id_to_round_id = {1, 0};
+          state.num_cards = {6, 7};
         }
-
-        private_info_sets["first"] = PrivateInfoSet();
-        private_info_sets["second"] = PrivateInfoSet();
+        private_info_sets[0] = PrivateInfoSet();
+        private_info_sets[1] = PrivateInfoSet();
         init_card_play();
       }
     } else {
@@ -246,16 +242,16 @@ public:
 
 private:
   GameState state;
-  std::map<std::string, PrivateInfoSet> private_info_sets;
-  std::map<std::string, std::shared_ptr<Player>> players;
+  std::vector<PrivateInfoSet> private_info_sets;
+  std::vector<std::shared_ptr<Player>> players;
   std::vector<int> deck;
-  std::string winner;
-  int round;
-  std::map<std::string, int> num_wins = {{"first", 0}, {"second", 0}};
+  int winner = -1;
+  int round = 1;
+  std::vector<int> num_wins = {0, 0};
   std::pair<GameState, PrivateInfoSet> active_player_info_set;
   std::default_random_engine random_engine;
 
-  std::string get_opp() { return state.acting_player_id == "first" ? "second" : "first"; }
+  int get_opp() { return 1 - state.acting_player_id; }
 
   std::vector<std::pair<int, std::vector<int>>> get_moves()
   {
