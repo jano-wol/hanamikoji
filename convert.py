3b99973b06b7accf4c2e01ba934882b718c0ee21
Janos Wolosz <janos.wolosz@gmail.com>
2025-05-20 13:24:36 +0200

    Improve convert script

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 import torch
 from hanamikoji.dmc.models_clean import LstmModel
 
-ckpt_path = "/home/jano/Repositories/hanamikoji/baselines/second.ckpt"  # or second, etc.
-
-state_dict = torch.load(ckpt_path, map_location="cpu")
-model = LstmModel()
-model.load_state_dict(state_dict)
-model.eval()
-scripted = torch.jit.script(model)
-scripted.save("second.pt")
+for p in ['first', 'second']:
+    ckpt_path = f"/home/jano/Repositories/hanamikoji/baselines/{p}.ckpt"
+    state_dict = torch.load(ckpt_path, map_location="cpu")
+    model = LstmModel()
+    model.load_state_dict(state_dict)
+    model.eval()
+    scripted = torch.jit.script(model)
+    scripted.save(f'./baselines/{p}.pt')
