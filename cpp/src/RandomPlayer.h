dcbcbb679a410a83c72142c6521d42a19c948082
Janos Wolosz <janos.wolosz@gmail.com>
2025-05-21 12:22:09 +0200

    Implement random player

--- cpp/src/RandomPlayer.h
+++ cpp/src/RandomPlayer.h
@@ -6,10 +6,10 @@
 class RandomAgent : public IPlayer
 {
 public:
-  int act(const GameState& /*gameState*/, const PrivateInfoSet& /*privateInfoSet*/) override
+  int act(const GameState& /*gameState*/, const PrivateInfoSet& privateInfoSet) override
   {
     static std::mt19937 rng{std::random_device{}()};
-    std::uniform_int_distribution<int> dist(0, 9);
+    std::uniform_int_distribution<size_t> dist(0, privateInfoSet.moves.size() - 1);
     return dist(rng);
   }
 };
