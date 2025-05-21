79da6f8c8053b1b680d5f79deeea628047f9f5f5
Janos Wolosz <janos.wolosz@gmail.com>
2025-05-21 12:30:47 +0200

    Fix

--- cpp/src/RandomAgent.h
+++ cpp/src/RandomAgent.h
@@ -1,15 +1,15 @@
-#ifndef HANAMIKOJI_RANDOM_PLAYER_H_INCLUDED
-#define HANAMIKOJI_RANDOM_PLAYER_H_INCLUDED
+#ifndef HANAMIKOJI_RANDOM_AGENT_H_INCLUDED
+#define HANAMIKOJI_RANDOM_AGENT_H_INCLUDED
 
 #include "IPlayers.h"
 
-class RandomPlayer : public IPlayer
+class RandomAgent : public IPlayer
 {
 public:
-  int act(const GameState& /*gameState*/, const PrivateInfoSet& /*privateInfoSet*/) override
+  int act(const GameState& /*gameState*/, const PrivateInfoSet& privateInfoSet) override
   {
     static std::mt19937 rng{std::random_device{}()};
-    std::uniform_int_distribution<int> dist(0, 9);
+    std::uniform_int_distribution<size_t> dist(0, privateInfoSet.moves.size() - 1);
     return dist(rng);
   }
 };
