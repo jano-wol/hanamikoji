9ac806b98d721198f52937b987e99892a38fb3e4
Janos Wolosz <janos.wolosz@gmail.com>
2025-05-22 22:07:02 +0200

    IMprove script

--- get_most_recent.sh
+++ get_most_recent.sh
@@ -2,11 +2,14 @@ checkpoint_dir=$1
 
 first_path=$first_dir`ls -v "$checkpoint_dir"first_weights* | tail -1`
 second_path=$second_dir`ls -v "$checkpoint_dir"second_weights* | tail -1`
+belief_path=$belief_dir`ls -v "$checkpoint_dir"belief_weights* | tail -1`
 
 echo $first_path
 echo $second_path
+echo $belief_path
 
 mkdir -p most_recent_model
 
 cp $first_path most_recent_model/first.ckpt
 cp $second_path most_recent_model/second.ckpt
+cp $belief_path most_recent_model/belief.ckpt
